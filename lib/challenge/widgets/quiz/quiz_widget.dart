a58391fa443ab71d97f8a1fc3ce30b2e9fc53f24
Pedro Henrique <pedrohfernandes2001@gmail.com>
2023-07-08 12:31:14 -0300

    finalizacao da aula 2 - criacao da challenge_page e widgets auxiliares

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,49 @@
+import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          AnswerWidget(
+            isRight: true,
+            isSelected: true,
+            title:
+                "Testando um texto aleatório enquanto eu digito sem olhar pro teclado",
+          ),
+          AnswerWidget(
+            isRight: false,
+            isSelected: true,
+            title:
+                "Testando um texto aleatório enquanto eu digito sem olhar pro teclado",
+          ),
+          AnswerWidget(
+            title:
+                "Testando um texto aleatório enquanto eu digito sem olhar pro teclado",
+          ),
+          AnswerWidget(
+            title:
+                "Testando um texto aleatório enquanto eu digito sem olhar pro teclado",
+          ),
+          AnswerWidget(
+            title:
+                "Testando um texto aleatório enquanto eu digito sem olhar pro teclado",
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -0,0 +1,52 @@
+import 'package:devquiz/core/core.dart';
+import 'package:devquiz/shared/widgets/progress_indicator_widget.dart';
+import 'package:flutter/material.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  const QuizCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 40,
+            width: 40,
+            child: Image.asset(AppImages.blocks),
+          ),
+          Text(
+            "Gerenciamento de Estado",
+            style: AppTextStyles.heading15,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Text(
+                  "3 de 10",
+                  style: AppTextStyles.body11,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: ProgressIndicatorWidget(
+                  value: 0.3,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
